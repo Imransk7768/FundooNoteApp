e88b9c7ca2cb444394908c59b46725574c98e31c
Imransk7768 <imransk7768@gmail.com>
2022-10-27 17:52:33 +0530

    create Web API

---
+++ FundooApp/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Interface;
+using CommonLayer.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FundooApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserBL iuserBL;
+        public UserController(IUserBL iuserBL)
+        {
+            this.iuserBL = iuserBL;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public IActionResult RegisterUser(UserRegistrationModel userResgistrationModel)
+        {
+            try
+            {
+                var result = iuserBL.Registration(userResgistrationModel);
+                if (result != null)
+                {
+                    return Ok(new { success = true, message = "Registration Is Successful", data = result });
+                }
+                else
+                {
+                    return BadRequest(new { success = false, message = "Registration Is Not Successful", data = result });
+                }
+
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ RepositoryLayer/Service/UserRL.cs
@@ -0,0 +1,50 @@
+﻿using CommonLayer.Model;
+using RepositoryLayer.Context;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RepositoryLayer.Service
+{
+    public class UserRL : IUserRL
+    {
+        private readonly FundooContext fundooContext;
+        //data of registered Dependency it
+        public UserRL(FundooContext fundooContext)
+        {
+            this.fundooContext = fundooContext;
+        }
+        public static UserEntity userEntity = new UserEntity();
+        public UserEntity Registration(UserRegistrationModel userRegistrationModel)
+        {
+            try
+            {
+                //UserEntity userEntity = new UserEntity();
+                userEntity.FirstName = userRegistrationModel.FirstName;
+                userEntity.LastName = userRegistrationModel.LastName;
+                userEntity.Email = userRegistrationModel.Email;
+                userEntity.Password = userRegistrationModel.Password;
+
+                fundooContext.Usertable.Add(userEntity);
+
+                int result = fundooContext.SaveChanges();
+
+                if (result != 0)
+                {
+                    return userEntity;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
